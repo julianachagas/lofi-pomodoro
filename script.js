ef4cca6f8ee5c12cc49819da9cb0bc7b16c4470d
Juliana Chagas <julianavrchagas@gmail.com>
2022-11-11 00:05:18 -0300

    feat: add pomodoro script

--- script.js
+++ script.js
@@ -1,3 +1,154 @@
+// POMODORO
+
+const timer = {
+  pomodoro: 1500,
+  shortBreak: 300,
+  longBreak: 900
+};
+let mode = 'pomodoro';
+let totalTime = 1500;
+let timeRemaining;
+let pomodoroInterval;
+let pomodoroRounds = 0;
+let pause = false;
+const controlBtn = document.querySelector('#control');
+
+const updateClock = time => {
+  let min = Math.floor(time / 60);
+  let sec = time - min * 60;
+  min = min < 10 ? `0${min}` : min;
+  sec = sec < 10 ? `0${sec}` : sec;
+  const element = document.querySelector('.clock');
+  element.textContent = `${min}:${sec}`;
+};
+
+const startTimer = () => {
+  timeRemaining = timer[mode];
+  if (pause) {
+    updateTimer();
+    pause = false;
+  }
+  pomodoroInterval = setInterval(updateTimer, 1000);
+  controlBtn.dataset.action = 'pause';
+  controlBtn.textContent = 'pause';
+};
+
+const stopTimer = () => {
+  clearInterval(pomodoroInterval);
+  controlBtn.dataset.action = 'start';
+  controlBtn.textContent = 'start';
+};
+
+const playNotification = () => {
+  let count = 1;
+  const notification = document.getElementById('notification');
+  notification.src = 'assets/sound-effects/notification.mp3';
+  notification.play();
+  notification.addEventListener('ended', () => {
+    if (count < 3) {
+      count++;
+      notification.play();
+    }
+  });
+};
+
+const updatePomodoroProgress = () => {
+  const progressBarPerimeter = 2 * Math.PI * 135;
+  const dashLength = (progressBarPerimeter * timeRemaining) / totalTime;
+  const pomodoroProgressBar = document.querySelector('.pomodoro-progress-bar');
+  // stroke-dasharray: first value = dash length, second value = gap length
+  pomodoroProgressBar.style.strokeDasharray = `${dashLength}, ${
+    progressBarPerimeter - dashLength
+  }`;
+  if (dashLength === 0) {
+    pomodoroProgressBar.style.strokeWidth = `0`;
+  }
+};
+
+const switchMode = newMode => {
+  mode = newMode;
+  totalTime = timer[mode];
+  const clock = document.querySelector('.clock');
+  clock.textContent =
+    mode === 'shortBreak' ? '05:00' : mode === 'longBreak' ? '15:00' : '25:00';
+  const menuBtns = document.querySelectorAll('.pomodoro-menu button');
+  menuBtns.forEach(button => {
+    button.classList.toggle('active', button.dataset.mode === mode);
+  });
+};
+
+const resetClock = () => {
+  timer.pomodoro = 1500;
+  timer.shortBreak = 300;
+  timer.longBreak = 900;
+  controlBtn.removeAttribute('disabled', '');
+  const pomodoroProgressBar = document.querySelector('.pomodoro-progress-bar');
+  pomodoroProgressBar.style.strokeWidth = `1rem`;
+  pomodoroProgressBar.style.strokeDasharray = `${Math.PI * 2 * 135}`;
+  pause = false;
+};
+
+const handleTimerEnd = () => {
+  stopTimer();
+  controlBtn.setAttribute('disabled', '');
+  playNotification();
+  setTimeout(() => {
+    resetClock();
+    if (mode !== 'pomodoro') {
+      switchMode('pomodoro');
+    } else {
+      pomodoroRounds++;
+      if (pomodoroRounds <= 3) {
+        switchMode('shortBreak');
+      } else {
+        switchMode('longBreak');
+        // reset pomodoro rounds, after the long break it'll restart the pomodoro circle
+        pomodoroRounds = 0;
+      }
+    }
+    startTimer();
+  }, 6000);
+};
+
+const updateTimer = () => {
+  timer[mode] = timeRemaining--;
+  if (timer[mode] === 0) {
+    handleTimerEnd();
+    return;
+  }
+  updateClock(timeRemaining);
+  updatePomodoroProgress();
+};
+
+controlBtn.addEventListener('click', e => {
+  const action = e.target.dataset.action;
+  if (action === 'start') {
+    startTimer();
+    // safari fix: safari doesn't play audio without a user interaction first
+    // play a silent audio on first interaction on page, before we actually need the sound effect, then we modify the audio src
+    const notification = document.getElementById('notification');
+    notification.src =
+      'data:audio/mpeg;base64,SUQzBAAAAAABEVRYWFgAAAAtAAADY29tbWVudABCaWdTb3VuZEJhbmsuY29tIC8gTGFTb25vdGhlcXVlLm9yZwBURU5DAAAAHQAAA1N3aXRjaCBQbHVzIMKpIE5DSCBTb2Z0d2FyZQBUSVQyAAAABgAAAzIyMzUAVFNTRQAAAA8AAANMYXZmNTcuODMuMTAwAAAAAAAAAAAAAAD/80DEAAAAA0gAAAAATEFNRTMuMTAwVVVVVVVVVVVVVUxBTUUzLjEwMFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQsRbAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQMSkAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV';
+    notification.play();
+  } else {
+    stopTimer();
+    pause = true;
+  }
+});
+
+// switch mode: pomodoro, short break, long break
+const pomodoroMenu = document.querySelector('.pomodoro-menu');
+pomodoroMenu.addEventListener('click', e => {
+  if (e.target.tagName !== 'BUTTON') return;
+  switchMode(e.target.dataset.mode);
+  stopTimer();
+  resetClock();
+  // reset the rounds when user manually switch modes
+  pomodoroRounds = 0;
+});
+
+// MUSIC PLAYER
+
 const songs = [
   {
     title: 'Spring Blossom',
